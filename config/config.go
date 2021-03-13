3cc6f37660129adea07947b30706de0a014243aa
Bradley Hilton <graywolf336@craftyn.com>
2021-03-12 19:04:11 -0600

    Add the loading of the config file

---
+++ config/config.go
@@ -0,0 +1,71 @@
+package config
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+//Config contains a reference to the configuration
+var Config *config
+
+type config struct {
+	System systemConfig `yaml:"system" json:"system"`
+}
+
+type systemConfig struct {
+	Port    int    `yaml:"port" json:"port"`
+	BaseURL string `yaml:"baseUrl" json:"baseUrl"`
+}
+
+func (c *config) load(filePath string) error {
+	yamlFile, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+
+	if err := yaml.Unmarshal(yamlFile, c); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *config) verifySettings() error {
+	if c.System.BaseURL == "" {
+		return errors.New("invalid system.baseUrl, can not be empty")
+	}
+
+	if strings.HasSuffix(c.System.BaseURL, "/") {
+		return errors.New("system.baseUrl must NOT end with '/'")
+	}
+
+	return nil
+}
+
+//HTTPHandler outputs the config as a json file
+func (c *config) HTTPHandler(w http.ResponseWriter) {
+	js, err := json.Marshal(c)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+//Load tries to load the configuration file
+func Load(filePath string) error {
+	Config = new(config)
+
+	if err := Config.load(filePath); err != nil {
+		return err
+	}
+
+	return Config.verifySettings()
+}
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"log"
+
+	"github.com/FideTech/yaus/config"
+)
 
 func main() {
 	log.Println("welcome to yet another url shortener (yaus)")
+
+	configFile := flag.String("configFile", "config.yaml", "Config File full path. Defaults to current folder")
+
+	flag.Parse()
+
+	if err := config.Load(*configFile); err != nil {
+		panic(err)
+	}
+
+	log.Printf("base url of %s", config.Config.System.BaseURL)
 }
